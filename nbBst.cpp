d69b0445ed3ab7c743992151558d7d641e3a976d
Varun <varun.1312@gmail.com>
2015-07-02 00:02:01 -0700

    Updated the print method and insert_tree method

--- nbBst.cpp
+++ nbBst.cpp
@@ -2,9 +2,10 @@
 #include <climits>
 
 #define MARK_NODE(pointer, status) (((uintptr_t)pointer & ~0x03) | status)
-#define ISNULL(pointer) (((uintptr_t)pointer & 0x03) == NULLPTR)
+#define ISNULL(pointer) (((uintptr_t)pointer & 0x03) == NULLPTR) 
+#define GET_DATA_PTR(pointer) (((Node *)((uintptr_t)pointer & ~0x03))->data)
 #define GET_DATA(pointer) *(int *)((uintptr_t)pointer & ~0x03)
-#define GET_ADDR(node, lr) (Node *)((uintptr_t)node->child[lr] & ~0x03)
+#define GET_ADDR(node) (Node *)((uintptr_t)node & ~0x03)
 #define CAS(ptr, source, sourceStatus, target, targetStatus) \
 	__sync_bool_compare_and_swap( \
 		ptr,\
@@ -49,6 +50,7 @@ public:
 		while(true) {
 			if (ISNULL(curr)) {
 				// Here actual insertion happens.
+				// Before inserting, we also need to check the validity of insertion.
 				int *predDataPointer = pred->data;
 				int predData = GET_DATA(predDataPointer);
 				Node *node = new Node(data);
@@ -69,7 +71,7 @@ public:
 						return insert_tree(root, data);
 				}
 			}
-			int *dataPointer = curr->data;
+			int *dataPointer = GET_DATA_PTR(curr);
 			int currData = GET_DATA(dataPointer);
 			if (currData == data) {
 				// Here we need to check if the node is marked or not.
@@ -77,11 +79,11 @@ public:
 				// Returning false for now.
 				return false;
 			}
-			else if (currData > data) {
-				pred = curr, curr = GET_ADDR(curr, RIGHT);
+			else if (data > currData) {
+				pred = GET_ADDR(curr), curr = curr->child[RIGHT];
 			}
-			else if (currData < data) {
-				ancNode = pred, pred = curr, curr = GET_ADDR(curr, LEFT);
+			else if (data < currData) {
+				ancNode = pred, pred = GET_ADDR(curr), curr = curr->child[LEFT]; 
 			}
 		}		
 	}
@@ -89,10 +91,28 @@ public:
 	bool insert(int data) {
 		return insert_tree(root, data);
 	}
+
+	void print_tree(Node *node) {
+		if (ISNULL(node))
+			return;
+		print_tree(node->child[LEFT]);
+		std::cout<<GET_DATA(GET_DATA_PTR(node))<<std::endl;
+		print_tree(node->child[RIGHT]);
+	}
+
+	void print() {
+		return print_tree(GET_ADDR(root->child[LEFT]));
+	}
 };
 
 void testbenchSequential() {
 	nbBst myTree;
+	myTree.insert(10);
+	myTree.insert(20);
+	myTree.insert(30);
+	myTree.insert(40);
+	myTree.insert(50);
+	myTree.print();
 }
 
 int main(void) {
