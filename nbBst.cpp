751748a0f1ce8161e995cbabf20411369c66b708
Varun <varun.1312@gmail.com>
2015-07-03 05:48:44 -0700

    Updated Macros in source code

--- nbBst.cpp
+++ nbBst.cpp
@@ -3,6 +3,11 @@
 #include <vector>
 #include <thread>
 
+#define ISNULL(ptr) (((uintptr_t)ptr & 0x03) == NULLPTR)
+#define ISSPLPTR(ptr) (ptr == NULL)
+#define GETDATA(ptr) *(int *)((uintptr_t)(((Node *)((uintptr_t)ptr & ~0x03))->dataPtr) & ~0x03)
+#define GETADDR(ptr) (Node *)((uintptr_t)ptr & ~0x03)
+
 enum status_t {
 	NORMAL,
 	MARKED,
@@ -31,6 +36,35 @@ private:
 public:	
 	nbBst() {
 		root = new Node(INT_MAX);
+	}
+
+	bool insert_tree(Node *startNode, int data) {
+		Node *pred = startNode;
+		Node *curr = startNode;
+		Node *ancNode = startNode;
+		int currData;
+		int ancNodeDataPrev, ancNodeDataCurr;
+		while(true) {
+			if (ISNULL(curr) || ISSPLPTR(curr)) {
+				// Here we will insert data.
+				return true;
+			}
+			currData = GETDATA(curr);
+			if (data > currData) {
+				pred = GETADDR(curr), curr = curr->child[RIGHT]; 
+			}
+			else if (data < currData) {
+				ancNode = pred, pred = GETADDR(curr), curr = curr->child[LEFT];
+			}
+			else if (data == currData) {
+				// Here check the validity of data;	
+				return false;
+			}
+		}
+	}
+	
+	bool insert(int data) {
+		return insert_tree(root, data);
 	}
 };
 
