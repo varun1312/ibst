adce308440ff4d43c3d194607ad08f4f77f7df65
Varun <varun.1312@gmail.com>
2015-07-03 05:19:35 -0700

    Updated node to contain unique null pointers

--- nbBst.cpp
+++ nbBst.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include <climits>
-
-#define MARK_NODE(pointer, status) (((uintptr_t)pointer & ~0x03) | status)
-#define ISNULL(pointer) (((uintptr_t)pointer & 0x03) == NULLPTR) 
-#define GET_DATA_PTR(pointer) (((Node *)((uintptr_t)pointer & ~0x03))->data)
-#define GET_DATA(pointer) *(int *)((uintptr_t)pointer & ~0x03)
-#define GET_ADDR(node) (Node *)((uintptr_t)node & ~0x03)
-#define IS_VALID(node) ()
-#define CAS(ptr, source, sourceStatus, target, targetStatus) \
-	__sync_bool_compare_and_swap( \
-		ptr,\
-		MARK_NODE(source, sourceStatus),\
-		MARK_NODE(target, targetStatus) )
-
+#include <vector>
+#include <thread>
 
 enum status_t {
 	NORMAL,
@@ -30,98 +19,26 @@ class nbBst {
 private:
 	class Node {
 	public:
-		int *data;
+		int *dataPtr;
 		Node *child[2];
 		Node(int data) {
-			this->data = new int(data);
-			this->child[LEFT] = this->child[RIGHT] = (Node *)MARK_NODE(this, NULLPTR);
+			this->dataPtr = new int(data);
+			child[LEFT] = child[RIGHT] = (Node *)((uintptr_t)data | NULLPTR);
 		}
 	};
+
 	Node *root;
-public:
+public:	
 	nbBst() {
-		std::cout<<"I am in BST constructor"<<std::endl;
 		root = new Node(INT_MAX);
-	}
-
-	bool insert_tree(Node *startNode, int data) {
-		Node *pred = startNode;
-		Node *curr = startNode;
-		Node *ancNode = startNode;
-		int ancNodeDataPrev, ancNodeDataCurr; 
-		while(true) {
-			if (ISNULL(curr)) {
-				// Here actual insertion happens.
-				// Before inserting, we also need to check the validity of insertion.
-				ancNodeDataCurr = GET_DATA(GET_DATA_PTR(ancNode));
-				if (ancNodeDataPrev != ancNodeDataCurr)
-					// This is to invalidate any on going operations. 
-					// We are restarting for now, but later, we must backtrack
-					// Also this code must be optimized.
-					return insert_tree(root, data);
-				int *predDataPointer = pred->data;
-				int predData = GET_DATA(predDataPointer);
-				Node *node = new Node(data);
-				if (data > predData) {
-					if (CAS(&pred->child[RIGHT], curr, NULLPTR, node, NORMAL))
-						return true;
-					else
-						/* This is the retrial point. Here we are restrating for now, but, we need to back track (or) help
-						in future */
-						return insert_tree(root, data);
-				}
-				else {
-					if (CAS(&pred->child[LEFT], curr, NULLPTR, node, NORMAL))
-						return true;
-					else
-						/* This is the retrial point. Here we are restrating for now, but, we need to back track (or) help
-						in future */
-						return insert_tree(root, data);
-				}
-			}
-			int *dataPointer = GET_DATA_PTR(curr);
-			int currData = GET_DATA(dataPointer);
-			if (currData == data) {
-				// Here we need to check if the node is marked or not.
-				// If it is marked, then we need to add the node again.
-				// Returning false for now.
-				return false;
-			}
-			else if (data > currData) {
-				pred = GET_ADDR(curr), curr = curr->child[RIGHT];
-			}
-			else if (data < currData) {
-				ancNode = pred, ancNodeDataPrev = GET_DATA(GET_DATA_PTR(ancNode)), pred = GET_ADDR(curr), curr = curr->child[LEFT]; 
-			}
-		}		
-	}
-	
-	bool insert(int data) {
-		return insert_tree(root, data);
-	}
-
-	void print_tree(Node *node) {
-		if (ISNULL(node))
-			return;
-		print_tree(node->child[LEFT]);
-		std::cout<<GET_DATA(GET_DATA_PTR(node))<<std::endl;
-		print_tree(node->child[RIGHT]);
-	}
-
-	void print() {
-		return print_tree(GET_ADDR(root->child[LEFT]));
 	}
 };
 
 void testbenchSequential() {
 	nbBst myTree;
-	srand(time(NULL));
-	for (int i = 0; i < 10; i++)
-		myTree.insert(rand());
-	myTree.print();
 }
 
-int main(void) {
+int main() {
 	testbenchSequential();
-	return 0;	
+	return 0;
 }
